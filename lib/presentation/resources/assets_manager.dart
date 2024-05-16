d113373e2105c9101acb40670e0e6c7ad354ba92
yara30999 <yaranaserelniny@gmail.com>
2024-05-16 12:07:51 +0300

    add splash screen using package flutter native splash

--- lib/presentation/resources/assets_manager.dart
+++ lib/presentation/resources/assets_manager.dart
@@ -1,14 +1,25 @@
 const String imagePath = "assets/images";
 const String jsonPath = "assets/json";
+const String svgPath = "assets/svg";
 
-// class ImageAssets {
-//   static const String splashLogo = "$imagePath/splash_logo.png";
-// }
+class ImageAssets {
+  static const String catInYelloCircle = "$imagePath/cat_in_yello_circle_2.png";
+  static const String blackCat = "$imagePath/cat_logo.png";
+  static const String pinkCat = "$imagePath/pink_cat.png";
+}
 
-// class JsonAssets {
-//   static const String loading = "$jsonPath/loading.json";
-//   static const String error = "$jsonPath/error.json";
-//   static const String empty = "$jsonPath/empty.json";
-//   static const String success = "$jsonPath/success.json";
-//   static const String successSlow = "$jsonPath/success_slow.json";
-// }
+class JsonAssets {
+  // static const String loading = "$jsonPath/loading.json";
+  // static const String error = "$jsonPath/error.json";
+  // static const String empty = "$jsonPath/empty.json";
+  // static const String success = "$jsonPath/success.json";
+  // static const String successSlow = "$jsonPath/success_slow.json";
+}
+
+class SvgAssets {
+  static const String catInYelloCircle2 = "$svgPath/cat_in_yello_circle_2.svg";
+  static const String catInYelloCircle = "$svgPath/cat_in_yello_circle.svg";
+  static const String blackCat = "$svgPath/cat_logo.svg";
+  static const String catWithCup = "$svgPath/cat_with_cup.svg";
+  static const String pinkCat = "$svgPath/pink_cat.svg";
+}
