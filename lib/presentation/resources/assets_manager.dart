69c1c8e14c965c1f558cec8f673275fc744023a5
yara30999 <yaranaserelniny@gmail.com>
2024-05-16 14:40:46 +0300

    addig some json animation

--- lib/presentation/resources/assets_manager.dart
+++ lib/presentation/resources/assets_manager.dart
@@ -9,11 +9,10 @@ class ImageAssets {
 }
 
 class JsonAssets {
-  // static const String loading = "$jsonPath/loading.json";
-  // static const String error = "$jsonPath/error.json";
-  // static const String empty = "$jsonPath/empty.json";
-  // static const String success = "$jsonPath/success.json";
-  // static const String successSlow = "$jsonPath/success_slow.json";
+  static const String loading = "$jsonPath/cat_loading.json";
+  static const String error = "$jsonPath/cat_error.json";
+  static const String empty = "$jsonPath/cat_empty.json";
+  static const String success = "$jsonPath/cat_success.json";
 }
 
 class SvgAssets {
